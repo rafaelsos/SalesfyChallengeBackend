2a3ab0b657e9fff487350b7a59aa0566042cc034
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-04-18 23:51:43 -0300

    retirado código comentado

--- src/routes/translate.route.ts
+++ src/routes/translate.route.ts
@@ -21,38 +21,6 @@ translateRouter.get('/translate/:id', (req, res) => {
   }
 
   return res.status(200).json(word);
-
-  /*
-  const typeNumber = Number.isInteger(number);
-
-  if (!typeNumber) {
-    return res.status(400).json({ error: 'Permitido somente numero inteiro' });
-  };
-
-  if (number < 0) {
-    return res.status(400).json({ error: 'Não é permitido traduzir um numero menor que zero' });
-  }
-
-  if (number > 999) {
-    return res.status(400).json({ error: 'Não é permitido traduzir um numero maior que 999' });
-  }
-  */
-
-  /*
-  if (number < 20) {
-    word = baseNumbers.base[number];
-  } else if (number < 100) {
-    const rest = number % 10;
-    const base = baseNumbers.base[number % 10];
-    const dozens = baseNumbers.dozens[Math.floor(number / 10)];
-
-    if (rest) {
-      word = `${dozens}-${base}`;
-    } else {
-      word = `${dozens}`;
-    }
-  }
-  */
 });
 
 export default translateRouter;
