256bb2cfcaab5d7fbfa689955394eae00de040ad
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-04-18 21:36:43 -0300

    ajuste test

--- src/__test__/translate.spec.ts
+++ src/__test__/translate.spec.ts
@@ -2,7 +2,6 @@ import app from '../app';
 import 'mocha';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-//import 'chai/register-should';
 
 chai.use(chaiHttp);
 
@@ -38,71 +37,3 @@ describe('test request ', () => {
       });
   });
 });
-
-/*
-describe('Number type test', () => {
-  it('numero tem que ser inteiro', (done) => {
-    let number = '1';
-    chai.request(app)
-      .get('/translate/' + number)
-      .send(number)
-      .end((res: any) => {
-        res.should.have.status(200);
-        res.body.name.should.equal('one');
-        res.body.should.have.property('_id').eql(number);
-        done();
-      });
-  }).timeout(8000);
-});
-*/
-/*
-describe('Minimum and maximum number validation', () => {
-  it('Minimum number 0', () => {
-    chai.request(app).get('/translate/-1')
-      .then((res: any) => {
-        chai.expect(res.status).to.eql(400);
-      });
-  }).timeout(8000);
-
-  it('Maximum number 999', () => {
-    chai.request(app).get('/translate/1000')
-      .then((res: any) => {
-        chai.expect(res.status).to.eql(400);
-      });
-  }).timeout(8000);
-});
-
-describe('Test response translate', () => {
-  it('Translate number 10', () => {
-    chai.request(app).get('/translate/10')
-      .then((res: any) => {
-        chai.expect(res.status).to.eql(200);
-        chai.expect(res.body).to.eql("ten");
-      });
-  }).timeout(8000);
-
-  it('Translate number 50', () => {
-    chai.request(app).get('/translate/50')
-      .then((res: any) => {
-        chai.expect(res.status).to.eql(200);
-        chai.expect(res.body).to.eql("fifty");
-      });
-  }).timeout(8000);
-
-  it('Translate number 200', () => {
-    chai.request(app).get('/translate/200')
-      .then((res: any) => {
-        chai.expect(res.status).to.eql(200);
-        chai.expect(res.body).to.eql("two thousand");
-      });
-  }).timeout(8000);
-
-  it('Translate number 999', () => {
-    chai.request(app).get('/translate/1011')
-      .then((res: any) => {
-        chai.expect(res.status).to.eql(200);
-        chai.expect(res.body).to.eql("nine hundred ninety-nine");
-      });
-  }).timeout(8000);
-});
-*/
